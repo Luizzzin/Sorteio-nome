849842ca766915fbd36bafbef93998f44a5cfc7b
Luizzzin <luizhenribarbodias@gmail.com>
2023-09-04 16:02:26 -0300

    up
    up

---
+++ main.js
@@ -0,0 +1,25 @@
+const btnSortear = document.querySelector('.sortear');
+const textarea = document.queryselector('.textarea');
+const tagResultado= document.querySelector('resultado p');
+const popUpResultado =document.querySelector('.resultado')
+
+function pegarDadosFormulario(){
+    const valorCampo = textarea.value;
+    const listaNomes = valorCampo.split(",");
+    const tamanhoArray = listaNome.length;
+    const posicaoNome = geraNumeroAleatorio(tamanhoArray);
+    const nome = listaNomes[posicaoNome]
+
+    mostraResultado(nome);
+    textarea.value = ""
+
+}
+btnSortear.addEventListener("click, pegarDadosFormulario")
+
+function geraNumeroAleatorio(tamanhoArray){
+    return Math.floor(Math.random()*(tamanhoArray - 0)+0)
+}
+function mostraResultado(nome){
+    tagResultado.innerHTML = `resultado ${nome}`
+    popUpResultado.classList.add(".resultado");
+}
